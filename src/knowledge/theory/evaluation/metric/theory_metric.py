094316b54b303b219e206dd62003511ed5d70953
Victor Guimarães <victorguimaraes13@gmail.com>
2020-06-11 20:59:30 +0100

    Added some theory metrics.
    Signed-off-by: Victor Guimarães <victorguimaraes13@gmail.com>

--- src/knowledge/theory/evaluation/metric/theory_metric.py
+++ src/knowledge/theory/evaluation/metric/theory_metric.py
@@ -1,10 +1,20 @@
 """
+The metrics to evaluate the system on examples.
+"""
+
+from typing import TypeVar, Generic, List, Tuple
+
+import sklearn
+
+"""
 Handles the theory metrics.
 """
 import math
+import sys
 from abc import abstractmethod
 
 from src.knowledge.examples import Examples, ExamplesInferences
+from src.language.language import Atom
 from src.util import Initializable
 
 
@@ -52,6 +62,9 @@ class TheoryMetric(Initializable):
 
         If they are equal, it should return 0.
 
+        The default implementation should perform well, given an appropriated
+        `compare` is implemented.
+
         :param candidate: the candidate evaluation
         :type candidate: float
         :param current: the current evaluation
@@ -90,10 +103,11 @@ class TheoryMetric(Initializable):
     def get_range(self):
         """
         Gets the range of the metric. The range of a metric is the difference
-        between the maximum and the minimum value a metric can assume.
+        between the maximum and the minimum value a metric can assume. If the
+        range is infinite, this is, the metric is unbounded, return `None`.
 
-        :return: the range of the metric
-        :rtype: float
+        :return: the range of the metric; or `None`, if the range is infinite
+        :rtype: float or None
         """
         pass
 
@@ -121,7 +135,6 @@ class TheoryMetric(Initializable):
         """
         pass
 
-    @abstractmethod
     def __eq__(self, other):
         if self == other:
             return True
@@ -138,42 +151,280 @@ class TheoryMetric(Initializable):
 
         return self.default_value == other.default_value
 
-    @abstractmethod
     def __hash__(self):
         return hash(
             (self.parameters_retrain_before_evaluate, self.default_value))
 
-    @abstractmethod
-    def __str__(self):
-        pass
-
-    @abstractmethod
     def __repr__(self):
+        return self.__class__.__name__
+
+
+J = TypeVar('J')  # Key type.
+K = TypeVar('K')  # Value type.
+
+
+class AccumulatorMetric(TheoryMetric, Generic[J, K]):
+    """
+    A template for metrics that simply accumulates a value for each proved
+    example.
+
+    J: The type of the initial value.
+    K: The type of the value that is accumulated to the initial value.
+    """
+
+    def __init__(self):
+        """
+        Creates an accumulator metric.
+        """
+        super().__init__()
+        self.ABSENT_PREDICTION_VALUE = 0.0
+
+    # noinspection PyMissingOrEmptyDocstring
+    def compute_metric(self, examples, inferred_values):
+        evaluation = self.calculate_evaluation(examples, inferred_values)
+        if evaluation is None:
+            return self.default_value
+        else:
+            return self.calculate_result(evaluation)
+
+    def calculate_evaluation(self, examples, inferred_values):
+        """
+        Calculates the internal evaluation of the inferred results over the
+        examples.
+
+        :param examples: the examples
+        :type examples: Examples
+        :param inferred_values: the inferred values
+        :type inferred_values: ExamplesInferences
+        :return: the accumulation of all the results
+        :rtype: J or None
+        """
+        if not inferred_values:
+            return None
+
+        result: J = self.initial_value()
+        for p, facts in examples.items():
+            inferred = inferred_values.get(p, dict())
+            for key, atom in facts.items():
+                prediction = inferred.get(key)
+                example_value: K = self.calculate_value(atom, prediction)
+                result = self.accumulate_value(result, example_value)
+
+        return result
+
+    @abstractmethod
+    def initial_value(self):
+        """
+        Gets the initial value. This value bust be the neutral element of the
+        `self.accumulate_value` method.
+
+        :return: the initial value
+        :rtype: J
+        """
+        pass
+
+    @abstractmethod
+    def accumulate_value(self, current, new):
+        """
+        Accumulates the `new` value into the `current` one.
+
+        :param current: the current value
+        :type current: J
+        :param new: the new value
+        :type new: K
+        :return: the accumulates value
+        :rtype: J
+        """
+        pass
+
+    @abstractmethod
+    def calculate_value(self, example, prediction):
+        """
+        Calculates the value that must be accumulated, based on the example
+        and its prediction.
+
+        :param example: the example
+        :type example: Atom
+        :param prediction: the prediction
+        :type prediction: float or None
+        :return: the value that must be accumulated
+        :rtype: K
+        """
+        pass
+
+    @abstractmethod
+    def calculate_result(self, result):
+        """
+        Calculates the final metric result, based on the accumulated result.
+
+        :param result: the accumulated result
+        :type result: J
+        :return: the final metric result
+        :rtype: float
+        """
         pass
 
 
-# TODO: implement this class and the other metrics
-class RocCurveMetric(TheoryMetric):
+class ListAccumulator(AccumulatorMetric[Tuple[List[float], List[float]],
+                                        Tuple[float, float]]):
+    """
+    Accumulates the true and prediction values in two lists in order to
+    compute curve metrics.
+    """
+
+    def __init__(self):
+        """
+        Creates a list accumulator.
+        """
+        super().__init__()
+        self.ABSENT_PREDICTION_VALUE = -sys.float_info.max
+        "Value to be used in the absence of the prediction of an example."
+
+    # noinspection PyMissingOrEmptyDocstring
+    def get_range(self):
+        return 1.0
+
+    # noinspection PyMissingOrEmptyDocstring
+    def get_maximum_value(self):
+        return 1.0
+
+    # noinspection PyMissingOrEmptyDocstring
+    def required_fields(self):
+        return []
+
+    # noinspection PyMissingOrEmptyDocstring
+    def initial_value(self):
+        return [], []
+
+    # noinspection PyMissingOrEmptyDocstring
+    def accumulate_value(self, current, new):
+        current[0].append(new[0])
+        current[1].append(new[1])
+
+        return current
+
+    # noinspection PyMissingOrEmptyDocstring
+    def calculate_value(self, example, prediction):
+        if prediction is None:
+            prediction = self.ABSENT_PREDICTION_VALUE
+
+        return example.weight, prediction
+
+    # noinspection PyMissingOrEmptyDocstring
+    @abstractmethod
+    def calculate_result(self, result):
+        pass
+
+
+class RocCurveMetric(ListAccumulator):
     """
     Computes the area under the ROC curve.
     """
 
     # noinspection PyMissingOrEmptyDocstring
+    def calculate_result(self, result):
+        return sklearn.metrics.roc_auc_score(*result)
+
+
+class PrecisionRecallCurveMetric(ListAccumulator):
+    """
+    Computes the area under the Precision-Recall curve.
+    """
+
+    # noinspection PyMissingOrEmptyDocstring
+    def calculate_result(self, result):
+        precision, recall, _ = sklearn.metrics.precision_recall_curve(*result)
+        return sklearn.metrics.auc(precision, recall)
+
+
+class LikelihoodMetric(AccumulatorMetric[float, float]):
+    """
+    Computes the likelihood of a theory, given the examples.
+
+    This class assumes that the predictions are probabilities in the
+    [0, 1] real interval.
+
+    The likelihood of a theory is measured by the product of the probability
+    of each example, given the theory. If the example is negative,
+    the complement of the probability of the example is used, instead.
+    """
+
+    EPSILON = 1.0e-4
+    """
+    The minimal value to be multiplied with the result. This prevents the 
+    that probability goes to `0` if a positive example is not proved or a 
+    negative one gets probability of `1`.
+    
+    This is also issued to check if an example is positive. An example is 
+    positive if its weight is less than EPSILON apart to `1`.
+    """
+
+    # noinspection PyMissingOrEmptyDocstring
     def required_fields(self):
-        pass
+        return []
 
     # noinspection PyMissingOrEmptyDocstring
-    def compute_metric(self, examples, inferred_values):
-        pass
+    def get_range(self):
+        return 1.0
 
     # noinspection PyMissingOrEmptyDocstring
-    def difference(self, candidate, current):
-        pass
+    def get_maximum_value(self):
+        return 1.0
 
     # noinspection PyMissingOrEmptyDocstring
-    def __eq__(self, other):
-        pass
+    def initial_value(self):
+        return 1.0
 
     # noinspection PyMissingOrEmptyDocstring
-    def __hash__(self):
-        pass
+    def accumulate_value(self, current, new):
+        return current * new
+
+    # noinspection PyMissingOrEmptyDocstring
+    def calculate_value(self, example, prediction):
+        if abs(example.weight - 1.0) > self.EPSILON:
+            prediction = 1.0 - prediction
+        return max(prediction, self.EPSILON)
+
+    # noinspection PyMissingOrEmptyDocstring
+    def calculate_result(self, result):
+        return result
+
+
+class LogLikelihoodMetric(LikelihoodMetric):
+    """
+    Computes the log likelihood of a theory, given the examples.
+
+    This class assumes that the predictions are probabilities in the
+    [0, 1] real interval.
+
+    The likelihood of a theory is measured by the sum of the log of the
+    probability of each example, given the theory. If the example is negative,
+    the complement of the probability of the example is used, instead.
+    """
+
+    def __init__(self):
+        """
+        Creates a log likelihood metric.
+        """
+        super().__init__()
+        self.default_value = -sys.float_info.max
+
+    # noinspection PyMissingOrEmptyDocstring
+    def get_range(self):
+        return None
+
+    # noinspection PyMissingOrEmptyDocstring
+    def get_maximum_value(self):
+        return 0.0
+
+    # noinspection PyMissingOrEmptyDocstring
+    def initial_value(self):
+        return 0.0
+
+    # noinspection PyMissingOrEmptyDocstring
+    def accumulate_value(self, current, new):
+        return max(current + new, self.default_value)
+
+    # noinspection PyMissingOrEmptyDocstring
+    def calculate_value(self, example, prediction):
+        return math.log(super().calculate_value(example, prediction))
